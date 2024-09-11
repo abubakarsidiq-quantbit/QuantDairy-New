2992501969af2c9b836b38777d30be4a6668d97b
Ksonali2002 <sonali.kinikarerpdata@gmail.com>
2024-09-11 14:34:37 +0530

    Variable Deduction

--- quantdairy/quantdairy/doctype/variable_deduction/variable_deduction.js
+++ quantdairy/quantdairy/doctype/variable_deduction/variable_deduction.js
@@ -1,14 +1,20 @@
 // Copyright (c) 2023, quantdairy and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Variable Deduction', {
-	// refresh: function(frm) {
-
-	// }
-});
+// frappe.ui.form.on('Variable Deduction', {
+	
+// });
 
 
 frappe.ui.form.on('Variable Deduction', {
+	refresh: function(frm){
+		if(frm.doc.is_deducted == 1){
+			frm.set_df_property("farmer_code", "read_only", 1);
+			frm.set_df_property("deduction_name", "read_only", 1);
+			frm.set_df_property("deduction_amount", "read_only", 1);
+			frm.set_df_property("warehouse_branch", "read_only", 1);
+		}
+	},
 	setup: function(frm) {
 		frm.set_query("farmer_code", function(doc) {
 			return {
--- quantdairy/quantdairy/doctype/variable_deduction/variable_deduction.py
+++ quantdairy/quantdairy/doctype/variable_deduction/variable_deduction.py
@@ -5,23 +5,24 @@ import frappe
 from frappe.model.document import Document
 
 class VariableDeduction(Document):
-	def on_submit(self):
-		payment=frappe.new_doc("Payment Entry")
-		payment.payment_type="Pay"
-		payment.mode_of_payment=self.mode_of_payment
-		payment.party_type="Supplier"
-		payment.party=self.farmer_code
-		payment.paid_amount=self.deduction_amount
-		payment.received_amount=self.deduction_amount
-		payment.paid_to=self.account
-		payment.paid_from=self.paid_from_account_company
-		if(self.reference_no):
-			payment.reference_no=self.reference_no
-		payment.insert()
-		self.payment_ref_link=payment.name
-		payment.docstatus=1
-		payment.custom_variable_deduction=self.name
-		payment.save()
+    pass
+	# def on_submit(self):
+	# 	payment=frappe.new_doc("Payment Entry")
+	# 	payment.payment_type="Pay"
+	# 	payment.mode_of_payment=self.mode_of_payment
+	# 	payment.party_type="Supplier"
+	# 	payment.party=self.farmer_code
+	# 	payment.paid_amount=self.deduction_amount
+	# 	payment.received_amount=self.deduction_amount
+	# 	payment.paid_to=self.account
+	# 	payment.paid_from=self.paid_from_account_company
+	# 	if(self.reference_no):
+	# 		payment.reference_no=self.reference_no
+	# 	payment.insert()
+	# 	self.payment_ref_link=payment.name
+	# 	payment.docstatus=1
+	# 	payment.custom_variable_deduction=self.name
+	# 	payment.save()
 
 	# def on_cancel(self):
 	# 	frappe.throw(str(self.payment_ref_link))
